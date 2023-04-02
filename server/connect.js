ab838eae774d7ec5cab82cb7582ee1a92bda908b
Keyur Panchal <keyur.np@somaiya.edu>
2023-04-02 13:40:09 +0530

    Login Register Logout Logic Done

---
+++ server/connect.js
@@ -0,0 +1,15 @@
+import mysql from "mysql";
+export const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "!!@@!!",
+    database: "social",
+    port: 3306
+});
+
+db.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+});
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+const app = express();
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+// middlewares
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.listen(8800, () =>{
+    console.log("API working on port 8800");
+})
+
+
