0a427e793181fd1e9111864298700a2f263766c2
Keyur Panchal <keyur.np@somaiya.edu>
2023-04-02 13:42:42 +0530

    Login Register Logout logic done

--- server/connect.js
+++ server/connect.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "!!@@!!",
+    password: "password",
     database: "social",
     port: 3306
 });
