97fa92884734033a8bad8a63fc2cc8d411589a94
Aryan <aryan04@somaiya.edu>
2023-04-03 14:06:10 +0530

    add post functionality

--- server/connect.js
+++ server/connect.js
@@ -1,5 +1,5 @@
 import mysql from "mysql";
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 dotenv.config();
 
 console.log(process.env.port);
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 import express from "express";
 const app = express();
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
+//route imports
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
+//routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
