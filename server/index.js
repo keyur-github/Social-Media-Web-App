d1abda47e953df8e447a2ccbe74e32f1ff5fc728
Aryan <aryan04@somaiya.edu>
2023-04-05 13:30:21 +0530

    relationship functionality

--- server/index.js
+++ server/index.js
@@ -5,13 +5,14 @@ dotenv.config();
 
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import multer from "multer";   
+import multer from "multer";
 //route imports
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationship.js";
 
 // middlewares
 app.use(express.json());
@@ -20,25 +21,26 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../client/public/upload')
+    cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname)
-  }
-})
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
   const file = req.file;
   res.status(200).json(file.filename);
-})
+});
 //routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
+app.use("/api/relationship", relationshipRoutes);
 
 app.listen(8800, () => {
   console.log("API working on port 8800");
